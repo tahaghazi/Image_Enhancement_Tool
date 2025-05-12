bda7bb37e1552f8715a388331fb2e37039de1364
2rkish <aliturkey469@gmail.com>
2025-05-12 16:07:11 +0300

    fixing spaces

--- MainCode.py
+++ MainCode.py
@@ -5,12 +5,8 @@ from skimage import io, exposure
 
 
 def Gamma(image, gamma=1.0):
-    # Build a lookup table mapping pixel values [0, 255] to their adjusted gamma values
-    gamma_inverse = 1.0 / gamma
-    table = np.array([((i / 255.0) ** gamma_inverse) * 255 for i in np.arange(0, 256)]).astype("uint8")
-    
-    # Apply gamma correction using the lookup table
-    return cv2.LUT(image, table)
+    image = image / 255.0
+    return cv2.pow(image, gamma)
 
 
 def Equalization(image):
